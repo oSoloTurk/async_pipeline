994ea8be8bc5132ddb7bfcfc39a2f79dd66da34f
Hakkı <hakki.ceylan2356@gmail.com>
2022-09-25 16:25:18 +0300

    implementation of thread worker

--- async_pipeline/worker.py
+++ async_pipeline/worker.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
-
+from threading import Thread
+from typing import Any, Optional
+from async_pipeline.iterator import ConsumableIterator
 
 class BaseWorker:
     """
@@ -8,8 +10,8 @@ class BaseWorker:
 
         self.status = 'idle' for default
     """
-    def __init__(self, *args, **kwargs):
-        self._status = 'idle'
+    def __init__(self):
+        self._status : str = 'idle'
 
     @property
     def status(self):
@@ -37,3 +39,77 @@ class BaseWorker:
     def start(self):
         """Start the worker"""
         raise NotImplementedError
+
+class ThreadWorker(BaseWorker):
+    """
+        ThreadWorker is a worker that runs in a thread.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iterator : ConsumableIterator = ConsumableIterator(
+            items=kwargs.get('items', []),
+            exits_if_empty=False
+        )
+        self._thread : Optional[Thread] = None
+
+    @abstractmethod
+    def process(self, pipeline_item, item):
+        """
+            This method is called by the worker to process the item.
+            This method should be implemented by the child class.
+        """
+        raise NotImplementedError
+
+    def _run(self):
+        for pipeline_item in self.iterator:
+            if self.status in ('stopped', 'idle'):
+                break
+            if pipeline_item is None:
+                continue
+            response : Optional[Any] = self.process(pipeline_item, pipeline_item.item)
+            pipeline_item.callback(response)
+
+    def pause(self):
+        """
+            Pause the worker
+            Workers listen to the status of the worker for pause and resume.
+        """
+        if self.status == 'running':
+            self._status = 'paused'
+        else:
+            raise Exception('Worker is not running')
+
+    def resume(self):
+        """
+            Pause the worker
+            Workers listen to the status of the worker for pause and resume.
+        """
+        if self.status == 'paused':
+            self._status = 'running'
+        else:
+            raise Exception('Worker is not paused')
+
+    def stop(self):
+        """Stop the worker"""
+        if self.status == 'running':
+            self._status = 'stopped'
+            self._stop()
+        else:
+            raise Exception('Worker is not running')
+
+    def start(self):
+        """Start the worker"""
+        if self.status == 'idle':
+            self._status = 'running'
+            self._start()
+        else:
+            raise Exception('Worker is not idle')
+
+    def _start(self):
+        self._thread = Thread(target=self._run)
+        self._thread.start()
+
+    def _stop(self):
+        self._thread.join()
+        self._thread = None
