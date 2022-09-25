304333dd41c25a93aa1e15a6feb8084fe0bd5ed0
Hakkı <hakki.ceylan2356@gmail.com>
2022-09-25 18:35:35 +0300

    async test utils

--- tests/utils/exception_tests.py
+++ tests/utils/exception_tests.py
@@ -6,3 +6,11 @@ def try_exception(excepted_exception, func, *args, **kwargs):
         pass
     else:
         raise AssertionError
+async def try_exception_async(excepted_exception, func, *args, **kwargs):
+    """Try exception"""
+    try:
+        await func(*args, **kwargs)
+    except excepted_exception:
+        pass
+    else:
+        raise AssertionError
