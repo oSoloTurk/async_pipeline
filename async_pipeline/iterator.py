06bd7be9146d9d9bab0c3586916d0a29632d59c7
Hakkı <hakki.ceylan2356@gmail.com>
2022-09-25 15:13:30 +0300

    implementation of consumable iterator

---
+++ async_pipeline/iterator.py
@@ -0,0 +1,39 @@
+"""Module using for more readabilty"""
+from typing import Iterable
+
+
+class BaseIterator(Iterable):
+    """Base class for iterators"""
+    def __iter__(self):
+        raise NotImplementedError
+
+    def append(self, item):
+        """Append item to the iterator"""
+        raise NotImplementedError
+
+
+class ConsumableIterator(BaseIterator):
+    """
+        ConsumableIterator is a generator that can be consumed iterated items
+    """
+    def __init__(self, items: list, exits_if_empty: bool = True):
+        self.items = list(items) # Take clone of the list
+        self.exits_if_empty = exits_if_empty
+
+    def __iter__(self):
+        while True:
+            try:
+                _item = self.items.pop()
+            except IndexError:
+                _item = None
+            finally:
+                yield _item
+            if not self.items and self.exits_if_empty:
+                break
+
+    def __len__(self):
+        return len(self.items)
+
+    def append(self, item):
+        """Append item to the end of the list"""
+        self.items.append(item)
