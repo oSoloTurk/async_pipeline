2c72129c4ffb577de57345405a11c30c528dc2b5
Hakkı <hakki.ceylan2356@gmail.com>
2022-09-25 16:44:34 +0300

    implementation of pipeline item

--- async_pipeline/structures.py
+++ async_pipeline/structures.py
@@ -17,3 +17,20 @@ class BasePipelineItem:
             This method is called when the pipeline item is done.
         """
         raise NotImplementedError
+
+class PipelineItem(BasePipelineItem):
+    """
+        PipelineItem is a pipeline item that can be processed by a worker.
+    """
+    def __init__(self, item:Optional[Any], callback:Optional[Callable] = None):
+        super().__init__(item, callback)
+
+        self.item = item
+        self.callback = callback
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        self.item.__call__(*args, **kwds)
+
+    def done(self, *args: Any, **kwds: Any) -> Any:
+        if self.callback:
+            self.callback(*args, **kwds)
